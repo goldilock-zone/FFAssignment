d39b90aaa3302af3ec1467eff83701e9f8fe676f
goldilock-zone <sarbajitghosh2002@gmail.com>
2023-03-13 08:11:12 +0530

    Adding plotly functionality

--- q3.py
+++ q3.py
@@ -1,7 +1,11 @@
+# from dash import Dash, dcc, html, Input, Output
+import plotly.graph_objects as go
+from stockstats import StockDataFrame
+
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('CL Data.csv')
+data = pd.read_csv('CL Data.csv')
 
 def manipulate(df : pd.DataFrame, n: int): #n is the lookback period
     #adding moving standard deviation
@@ -14,11 +18,27 @@ def manipulate(df : pd.DataFrame, n: int): #n is the lookback period
     high_close = np.abs(df['High'] - df['Close'].shift())
     low_close = np.abs(df['Low'] - df['Close'].shift())
 
-    df['True Range'] = (df['High'] - df['Low']), abs(df['High'] - df['Close']), abs(df['Low'] - df['Close']))
+    tr = np.array(np.maximum(low_close, np.maximum(high_low, high_close)))
+
+    df['True Range'] = tr
     
     df['ATR'] = df['True Range'].rolling(n).mean()
 
     return df
 
-df = manipulate(df, 10)
-print(df)
+df = manipulate(data, 10)
+
+fig = go.Figure(data=[go.Candlestick(x=df['Timestamp'],
+                open=df['Open'],
+                high=df['High'],
+                low=df['Low'],
+                close=df['Close'],
+                line = dict(width=1)
+                )])
+
+df_ti = StockDataFrame.retype(df)
+print(df_ti)
+fig.add_trace(go.Line(x=df['timestamp'],y=df_ti["atr"],))
+fig.add_trace(go.Line(x=df['timestamp'],y=df_ti["rolling close standard deviation"],))
+
+fig.show()
